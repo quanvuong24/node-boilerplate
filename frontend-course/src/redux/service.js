586885ab36fdb32ae27ea25a6a6d7d62d865d9ca
Quan Vuong <quan.vuong@hotschedules.com>
2018-08-30 23:43:38 +0700

    change url style

--- frontend-course/src/redux/service.js
+++ frontend-course/src/redux/service.js
@@ -4,10 +4,10 @@ import {
   fetch_register, fetch_register_error, fetch_register_success,
   is_authenticate, 
  } from './actions';
-export const be_url = process.env.BE_LB;
+var be_url = process.env.BE_LB;
 export const be_pro = 'http://';
 export const TOKEN = 'token';
-export const URL_SERVER = `${be_pro}${be_url}`;
+export const URL_SERVER = `${be_pro}` + be_url;
 const URL_SERVER_API = `${URL_SERVER}/api`;
 
 const fetchApi = (url, method = 'GET', body = null) => {
